45425b3f3bd661b1c74251d52238da3bdec5b370
Robinson Shin <robinsonshin@ou.edu>
2018-11-17 01:13:17 -0600

    Finished all the tests

--- OvalTestOfficial.java
+++ OvalTestOfficial.java
@@ -7,9 +7,9 @@ import org.junit.Test;
 /**
  * Test cases for the Oval and the Circle
  * 
- * @author Stephen
- * @version 2018-04-02
- * Lab 11
+ * @author Stephen, Robinson Shin
+ * @version 2018-11-17
+ * Lab 12
  */
 public class OvalTestOfficial
 {
@@ -34,6 +34,14 @@ public class OvalTestOfficial
 
         // Verify the location for each shape
         // TODO: implement this.
+        Point[] test = new Point[2];
+        test[0] = new Point(100,100);
+        test[1] = new Point(200,300);
+        
+        Assert.assertEquals("Incorrect location for the Oval", test[0],o1.getLocation()[0]);
+        Assert.assertEquals("Incorrect location for the Oval", test[0],o2.getLocation()[0]);
+        Assert.assertEquals("Incorrect location for the Circle", test[1],c1.getLocation()[0]);
+        Assert.assertEquals("Incorrect location for the Circle", test[1],c2.getLocation()[0]);
 
         // Verify the colors for each shape
         Assert.assertEquals("Incorrect color for the Oval", Color.BLACK, o1.getColor());
@@ -63,4 +71,40 @@ public class OvalTestOfficial
     }
     
     // TODO: test color and fill in constructor.
+    /**
+     * Unfilled constructor
+     */
+    @Test
+    public void testOvalConstructorUnfilled()
+    {
+        Assert.assertFalse("Oval fill incorrect.", o2.isFilled());
+        Assert.assertFalse("Circle fill incorrect.", c2.isFilled());
+    }
+    
+    /**
+     * Filled constructor
+     */
+    @Test
+    public void testOvalConstructorFilled()
+    {
+        Assert.assertTrue("Oval fill incorrect.", o1.isFilled());
+        Assert.assertTrue("Circle fill incorrect.", c1.isFilled());
+    }
+    
+    /**
+     * Color constructor
+     */
+    @Test
+    public void testOvalConstructorColor()
+    {
+        Color clr1 = o1.getColor();
+        Color clr2 = o2.getColor();
+        Color clr3 = c1.getColor();
+        Color clr4 = c2.getColor();
+        
+        Assert.assertEquals("Oval color incorrect.", Color.BLACK, clr1);
+        Assert.assertEquals("Oval color incorrect.", Color.CYAN, clr2);
+        Assert.assertEquals("Circle color incorrect.", Color.BLUE, clr3);
+        Assert.assertEquals("Circle color incorrect.", Color.RED, clr4);
+    }
 }
